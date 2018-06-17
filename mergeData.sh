56bc9e6f3d3e5a893a43897997654f6f741c562f
Ran Wang <randombazinga@gmail.com>
2018-06-17 12:50:26 -0400

    clear code before while loop

--- mergeData.sh
+++ mergeData.sh
@@ -5,60 +5,72 @@ test=$(find . -maxdepth 1 -type d -regextype sed -regex ".*" | cut -c 3-)
 
 echo $test
 
-echo "no sort"
+echo "Analysing your postProcessing folder ..."
 a=($(echo "$test" | tr ',' '\n' ))
 
-echo "${a[0]}"
-echo "${a[1]}"
-echo "${a[2]}"
-echo "${a[3]}"
-echo "======================================"
-echo "after sort"
+# echo "${a[0]}"
+# echo "${a[1]}"
+# echo "${a[2]}"
+# echo "${a[3]}"
+# echo "======================================"
+# echo "after sort"
 
 a=($(echo "$test" | tr ',' '\n' | sort -g ))
 
+echo "You have: ${#a[@]} time folders:"
+
 echo "${a[0]}"
 echo "${a[1]}"
 echo "${a[2]}"
 echo "${a[3]}"
 
-echo "lenth of the array: "
-echo "${#a[@]}"
+# echo "lenth of the array: "
+# echo "${#a[@]}"
 
-#### loop 1
+echo "Creating the tmp data"
+
+cd "${a[0]}"
+cp -f ./forceCoeffs_bins.dat ../forceCoeffs_bins.dat.tmp
+cp -f ./forceCoeffs.dat ../forceCoeffs.dat.tmp
+cd ..
+
+pwd
+
+#### loop inital: find the latest time in the 0 folder
 
 cd "${a[0]}"
 
-cp -f ./forceCoeffs_bins.dat ../forceCoeffs_bins.dat.tmp
-cp -f ./forceCoeffs.dat ../forceCoeffs.dat.tmp
+echo "I am in the folder: "
+
+pwd
 
 latestTime=$(tac forceCoeffs.dat |egrep -m 1 . | head -n1 | awk '{print $1;}')
 
-echo $latestTime
+echo "The latest time in this time folder is $latestTime"
 
 nextLine=1
-pwd
 
 cd ..
 
-
+######## loop 1
 
 cd "${a[1]}"
 
+pwd
+
+latestTime=$(tac ../forceCoeffs.dat.tmp |egrep -m 1 . | head -n1 | awk '{print $1;}')
+
 latestTimeLineNum=$(grep -nw $latestTime forceCoeffs.dat |gawk '{print $1}' FS=":")
 
-
-echo "The latestTime is :"
-
-echo $latestTimeLineNum
 (( latestTimeLineNum += nextLine))
-echo $latestTimeLineNum
+
+echo "The latestTime is at line : $latestTimeLineNum"
 
 sed -n "$latestTimeLineNum,$ p" forceCoeffs.dat >> ../forceCoeffs.dat.tmp
 
 cd ..
 
-#### try to loop
+######## loop 2
 
 cd "${a[2]}"
 
@@ -66,15 +78,11 @@ pwd
 
 latestTime=$(tac ../forceCoeffs.dat.tmp |egrep -m 1 . | head -n1 | awk '{print $1;}')
 
-echo $latestTime
-
 latestTimeLineNum=$(grep -nw $latestTime forceCoeffs.dat |gawk '{print $1}' FS=":")
 
-echo "==="
-
-echo $latestTimeLineNum
 (( latestTimeLineNum += nextLine))
-echo $latestTimeLineNum
+
+echo "The latestTime is at line : $latestTimeLineNum"
 
 sed -n "$latestTimeLineNum,$ p" forceCoeffs.dat >> ../forceCoeffs.dat.tmp
 
@@ -88,14 +96,16 @@ pwd
 
 latestTime=$(tac ../forceCoeffs.dat.tmp |egrep -m 1 . | head -n1 | awk '{print $1;}')
 
-echo $latestTime
-
 latestTimeLineNum=$(grep -nw $latestTime forceCoeffs.dat |gawk '{print $1}' FS=":")
 
-echo "==="
-
-echo $latestTimeLineNum
 (( latestTimeLineNum += nextLine))
-echo $latestTimeLineNum
+
+echo "The latestTime is at line : $latestTimeLineNum"
 
 sed -n "$latestTimeLineNum,$ p" forceCoeffs.dat >> ../forceCoeffs.dat.tmp
+
+cd ..
+
+pwd
+
+echo "Done"
